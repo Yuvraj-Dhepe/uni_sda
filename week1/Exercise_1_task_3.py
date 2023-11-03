7033f89b4da1d83f1c989525eed2a87875f1859f
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2023-11-03 08:04:13 +0100

    Added names to .py\ file of jupyter nb

--- week1/Exercise_1_task_3.py
+++ week1/Exercise_1_task_3.py
@@ -3,7 +3,7 @@
 
 # In[1]:
 
-
+# Group Members: Dhvaniben Jasoliya, Leutrim Uka, Tauqeer Rumaney, Nicola Horst, Yuvraj Dhepe
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
