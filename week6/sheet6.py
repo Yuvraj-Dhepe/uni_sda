6500f0bdc2f91884e639b60324e5399d00385223
nicola horst <nicola.horst@sap.com>
2023-12-07 10:54:16 +0100

    added comments to task 2

--- week6/sheet6.py
+++ week6/sheet6.py
@@ -1,20 +1,12 @@
 import numpy as np
 
 
-def get_z_0(m: int):
-    return np.random.normal(loc=0, scale=0.5, size=m)
-
-
 def get_eps(m):
     return np.random.normal(0, 0.5, size=(m, 1))
 
 
 def evolution(z_n):
     return 0.99 * z_n + get_eps(m=len(z_n))
-
-
-def get_observations(z_n):
-    return z_n + get_eps(1)
 
 
 def mse(y, _y):
@@ -44,17 +36,17 @@ def kalman_filter(initial_mean, initial_cov, evolution_noise, evolution_coef, ob
     state_covariances = np.zeros(num_steps)
 
     # Initialization of the mean and the variances
-    state_mean = initial_mean
-    state_covariance = initial_cov
+    state_mean = initial_mean  # m_0
+    state_covariance = initial_cov  # C_0
 
     for step in range(num_steps):
         # 1. produce prediction
-        predicted_state_mean = evolution_coef * state_mean
+        predicted_state_mean = evolution_coef * state_mean  # M^-1 0.99
         predicted_state_covariance = evolution_coef ** 2 * state_covariance + evolution_noise ** 2
 
         # 2 update the model by using the new observation to compute the kalman gain
         k_gain = predicted_state_covariance / (predicted_state_covariance + evolution_noise ** 2)
-        state_mean = predicted_state_mean + (k_gain * (observations[step] - predicted_state_mean))
+        state_mean = predicted_state_mean - (k_gain * (predicted_state_mean - observations[step]))
         state_covariance = predicted_state_covariance - (k_gain * predicted_state_covariance)
 
         state_means[step] = state_mean
@@ -77,7 +69,8 @@ def ensemble_kalman_filter(obs, initial_ensemble, num_steps):
 
         # Update step
         kalman_gain = np.cov(ensemble, rowvar=False) / (np.cov(ensemble, rowvar=False) + 0.5 ** 2)
-        ensemble = ensemble + kalman_gain * (obs[step] - ensemble)
+
+        ensemble = ensemble - kalman_gain * (ensemble - obs[step])
 
         # compute the mean for the ensemble
         state_means[step, :] = np.mean(ensemble, axis=1)
@@ -86,30 +79,29 @@ def ensemble_kalman_filter(obs, initial_ensemble, num_steps):
 
 
 if __name__ == "__main__":
-    data_path: str = "./Data/reference_signal.txt"
+    data_path: str = "../reference_signal.txt"
+    obs_path: str = "../data.txt"
 
     # load the true signal
     signal: list = []
+    observations: list = []
     with open(data_path) as reference_txt:
         for line in reference_txt:
             signal.append(float(line))
+
+    with open(data_path) as reference_txt:
+        for line in reference_txt:
+            observations.append(float(line))
 
     # Define the number of steps
     num_steps: int = len(signal)
 
     # Generate true signal and observations
     true_signal = np.array(signal)
-    observations = np.zeros(num_steps)
-
-    # Initialization
-    observations[0] = true_signal[0] + np.random.normal(0, 0.5)
-
-    # Generate true signal and observations
-    for step in range(1, num_steps):
-        observations[step] = get_observations(true_signal[step])
+    observations = np.array(observations)
 
     # Kalman Filter
-    kf_means, _ = kalman_filter(observations=observations, initial_mean=0, initial_cov=0.5,evolution_coef=0.99, evolution_noise=0.5, num_steps=num_steps)
+    kf_means, _ = kalman_filter(observations=observations, initial_mean=0, initial_cov=0.5, evolution_coef=0.99, evolution_noise=0.5, num_steps=num_steps)
 
     # Ensemble Kalman Filter with different ensemble sizes
     ensemble_sizes = [5, 10, 25, 50]
@@ -124,3 +116,17 @@ if __name__ == "__main__":
     print(f"Mean Squared Error of Kalman Filter: {mse_kal}")
     for ensemble_size, mse_enkf in zip(ensemble_sizes, mse_enkfs):
         print(f"Mean Squared Error of Ensemble Kalman Filter with Ensemble Size {ensemble_size}: {mse_enkf}")
+
+
+    print(
+        "\n\n"
+        "Comments:\n"
+        "It Appears, that the Kalman Filter performs a bit better than the Ensemble Kalman Filter. \n"
+        "This can have different reasons. One would be the simplicity of the data. Ensemble Kalman Filters are mostly \n"
+        "used when the data has high dimensionality (e.g. 10e8). However in this case the dimensionality of the data is 1. \n"
+        "Another reason could be that the number of Ensemble members is rather small. It is observable that the MSE \n"
+        "for an increasing number of members is becoming smaller. \n"
+        "So increasing the number of ensemble members does improve the performance. \n"
+        "At the end it is hard to say why exactly the performance is worse but it will be a combination of the above \n"
+        "mentioned facts"
+    )
